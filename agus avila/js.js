925d7233641e33044abf3a58950a5da806bd253b
agustin-213 <136341304+agustin-213@users.noreply.github.com>
2023-08-07 10:08:36 -0300

    Add files via upload

---
+++ agus avila/js.js
@@ -0,0 +1,42 @@
+/*todo esto es para practicar en clase va a estar subido
+voy a poder buscar info de js en las clases de yo puedo programar y otras paginas*/
+console.log (2+2)
+console.log (2-2)
+console.log (2*2)
+console.log (2/2)
+
+let suma = 2+2
+let resta = 2-2
+let multiplicacion = 2*2
+let division = 2/2
+
+console.log (suma)
+console.log (resta)
+console.log (multiplicacion)
+console.log (division)
+
+alert (suma)
+alert (resta)
+alert (multiplicacion)
+alert (division)
+alert ('mi edad es')
+alert (2023-2008)
+ 
+let añoNacimiento = prompt ('¿cuando naciste?',);
+let añoActual = prompt ('¿que año es?',)
+alert(añoActual-añoNacimiento); 
+prompt ('¿Esa Era Tu Edad?')
+/*esto va a pedir el año de nacimiento y el año actual luego va a dar la edad actual y le va a preguntar si es correcto o no*/
+let nombre1 = prompt ('¿Cual es tu nombre?')
+let H = ('hola ')
+alert(H+nombre1)
+
+let nombreAlumno = ('agustin ')
+let apellidoAlumno = ('avila')
+console.log (H+nombreAlumno+apellidoAlumno)
+alert (H+nombreAlumno+apellidoAlumno)
+let diez = 10
+let tres = 3
+let resultadoMultiplicacion = (diez*tres)
+alert (resultadoMultiplicacion)
+console.log (resultadoMultiplicacion)
